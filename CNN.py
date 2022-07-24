5250d4942c11cae83a669262b6adcac011495da9
Jujie-YANG <Yangjujie3619@outlook.com>
2022-07-24 20:01:03 +0100

    next time continue from P28 in bilibili

--- CNN.py
+++ CNN.py
@@ -56,7 +56,7 @@ feature_extract = True
 
 # Train on GPU or CPU
 train_on_gpu = torch.cuda.is_available()
-
+print(train_on_gpu)
 if not train_on_gpu:
     print('CUDA is not available.  Training on CPU ...')
 else:
--- text/run.py
+++ text/run.py
@@ -6,6 +6,7 @@ from importlib import import_module
 import argparse
 from tensorboardX import SummaryWriter
 
+# --model TextRNN
 parser = argparse.ArgumentParser(description='Chinese Text Classification')
 parser.add_argument('--model', type=str, required=True, help='choose a model: TextCNN, TextRNN, FastText, TextRCNN, TextRNN_Att, DPCNN, Transformer')
 parser.add_argument('--embedding', default='pre_trained', type=str, help='random or pre_trained')
@@ -28,7 +29,7 @@ if __name__ == '__main__':
         from utils import build_dataset, build_iterator, get_time_dif
 
     x = import_module('models.' + model_name)
-    config = x.Config(dataset, embedding)
+    config = x.Config(dataset, embedding) 
     np.random.seed(1)
     torch.manual_seed(1)
     torch.cuda.manual_seed_all(1)
